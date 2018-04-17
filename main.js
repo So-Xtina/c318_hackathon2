d24e1e8a025ae9818bf75a4b27bf7c955b200cfd
ddonjuan <daniel.donjuan25@gmail.com>
2018-04-17 12:25:02 -0700

    JS docs form

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
-//***************************   Corey  *******************************************
+$(document).ready(initializeApp);
+
+var destinationArray = [];
+/*************************************************************************************************************************
+ *initializeApp
+ * *@params {undefined} none
+ * @returns: {undefined} none
+ * initializes the application, including adding click handlers and pulling in any data from the server, in later versions
+ */
+function initializeApp(){
+
+}
 
 
+/*************************************************************************************************************************
+ * addClickHandlersToElements
+ * @params {undefined}
+ * @returns  {undefined}
+ */
+
+function addClickHandlersToElements(){
 
 
+}
 
+/*************************************************************************************************************************
+ * addDestination
+ * @params {undefined}
+ * @returns  {undefined}
+ */
 
+function addDestination(){
+    var destinationObject = {};
 
-//***************************   Daniel  *******************************************
+}
 
+/*************************************************************************************************************************
+ * renderMapToDom
+ * @params {object}
+ * @calls Google Maps API call
+ * @returns  {undefined}
+ */
 
+function renderMapToDom(destinationObject){
 
+}
 
+/*************************************************************************************************************************
+ * carousalModal
+ * @params {destinationObject}
+ * @calls renderPicturesToModal,
+ * @returns  {undefined}
+ */
 
+function carousalModal(destinationObject){
 
+}
 
+function googleMapsLocations(){
 
-
-//***************************   Kristina    *******************************************
-
-
-
-
-
-
-
-
-//***************************   Mark    *******************************************
+}
