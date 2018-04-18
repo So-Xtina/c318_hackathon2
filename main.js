5fca934335100e2279b1df9b2e1d641017c5cb44
Legacy92 <colemancorey22@gmail.com>
2018-04-17 20:58:54 -0700

    added ideantifier to the div allowing selection of video onclick

--- main.js
+++ main.js
@@ -131,25 +131,28 @@ function initializeMap(locationsArray) {
  * YouTube AJAX Call:
  */
 // 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+    var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var player;
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'Y5bW1frdYOY',
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+    var player;
+    function onYouTubeIframeAPIReady(array) {
+        for(let i = 0; i<array.length; i++){
+            player= new YT.Player('player', {
+                height: '390',
+                width: '640',
+                videoId: 'Y5bW1frdYOY', // needs to be a function looping through the object and pulling the videoId value and placing the correct one here for the call
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                }
+            });
         }
-    });
+    }
 }
 
 // 4. The API will call this function when the video player is ready.
